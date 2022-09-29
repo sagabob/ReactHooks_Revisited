d0ec4e729f757068258e77d03c8822545247c839
DESKTOP-H29UOQ1\bobpham <bobpham.tdp@gmail.com>
2022-09-29 22:02:59 +1000

    add examples of why we need useCallback

---
+++ src/problems/useCallback/01/CounterWithCallback.tsx
@@ -0,0 +1,69 @@
+import React, { useCallback, useState } from "react";
+import { Box, Button, Heading, VStack } from "@chakra-ui/react";
+
+const funcCount = new Set();
+
+export default function () {
+  const [count, setCount] = useState(0);
+  const [number, setNumber] = useState(0);
+
+  const incrementCounter = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
+  
+  const decrementCounter = useCallback(() => {
+    setCount(count - 1);
+  }, [count]);
+  
+  const incrementNumber = useCallback(() => {
+    setNumber(number + 1);
+  }, [number]);
+
+  const decrementNumber = useCallback(() => {
+    setNumber(number - 1);
+  }, [number]);
+
+  funcCount.add(incrementCounter);
+  funcCount.add(decrementCounter);
+  funcCount.add(incrementNumber);
+  funcCount.add(decrementNumber);
+  console.log(funcCount);
+
+  return (
+    <>
+      <VStack
+        shadow="md"
+        borderWidth="1px"
+        padding="5px"
+        w="500px"
+        backgroundColor="yellow.200"
+      >
+        <Heading fontSize="md">Multple Counters</Heading>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          width="100%"
+          py={5}
+          px={5}
+          gap={5}
+        >
+          <Button colorScheme="blackAlpha" onClick={incrementCounter}>
+            +
+          </Button>
+          <Button colorScheme="blackAlpha" onClick={decrementCounter}>
+            -
+          </Button>
+          Count: {count}
+          <Button colorScheme="blackAlpha" onClick={incrementNumber}>
+            +
+          </Button>
+          <Button colorScheme="blackAlpha" onClick={decrementNumber}>
+            -
+          </Button>
+        </Box>
+        <footer>Msg: Function Size = {funcCount.size}</footer>
+      </VStack>
+    </>
+  );
+}
